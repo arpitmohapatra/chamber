0be81425f39091010e9bca2db23be42c9a0cd2f1
Arpit Mohapatra <arpitmohapatra18@gmail.com>
2025-12-09 23:00:19 -0500

    Fix PWA config for github pages

--- vite.config.js
+++ vite.config.js
@@ -17,15 +17,17 @@ export default defineConfig({
                 background_color: '#0f0f1e',
                 display: 'standalone',
                 orientation: 'portrait-primary',
+                start_url: '/chamber/',
+                scope: '/chamber/',
                 icons: [
                     {
-                        src: '/icons/icon-192x192.png',
+                        src: 'icons/icon-192x192.png',
                         sizes: '192x192',
                         type: 'image/png',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/icons/icon-512x512.png',
+                        src: 'icons/icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'any maskable'
